833fb378606405a07774edc1293386927823dfbb
cissty <128071188+cissty@users.noreply.github.com>
2023-04-16 05:58:57 +0300

    updated code, need css

--- script.js
+++ script.js
@@ -8,72 +8,128 @@
 
 
 //CREATING A RANDOM VALUE FOR EACH ITEM//
-function getComputerChoice(){
+function getComputerChoice() {
 
     let randomGenerator = Math.random()
 
-    if (randomGenerator <= 1 / 3 && randomGenerator >= 0){
+    if (randomGenerator <= 1 / 3 && randomGenerator >= 0) {
         return 'rock';
-    }else if (randomGenerator < 2 / 3 && randomGenerator >= 1 / 3){
+    } else if (randomGenerator < 2 / 3 && randomGenerator >= 1 / 3) {
         return 'paper';
-    }else if (randomGenerator < 3 / 3 && randomGenerator >= 2 / 3){
+    } else if (randomGenerator < 3 / 3 && randomGenerator >= 2 / 3) {
         return 'scissors';
     }
 }
 
 //PLAYING a single ROUND of rock, paper, scissors and selecting who wins or lose//
 
-function singleRound(playerSection, computerSection){    
-        if(playerSection === 'rock'){
-        if (computerSection === 'scissors'){
-            return `You win! ${playerSection} beats ${computerSection}`;
-        }else if(computerSection === 'rock'){
-            return `You picked ${playerSection} and computer picked ${computerSection}. It\'s a TIE!`;
-        }else {
-            return `You lose! ${computerSection} beats ${playerSection}`;
-        }
-    }
-        else if(playerSection === 'scissors'){
-        if (computerSection === 'scissors' ){
-            return `You picked ${playerSection} and computer picked ${computerSection}. It\'s a TIE!`;
-        }else if(computerSection === 'paper'){
-            return `You win! ${playerSection} beats ${computerSection}`;
-        }else {
-            return `You lose! ${computerSection} beats ${playerSection}`;
-        }
-    }
-        else if(playerSection === 'paper'){
-        if (computerSection === 'paper' ){
-            return `You picked ${playerSection} and computer picked ${computerSection}. It\'s a TIE!`;
-        }else if(computerSection === 'rock'){
-            return `You win! ${playerSection} beats ${computerSection}`;
-        }else {
-            return `You lose! ${computerSection} beats ${playerSection}`;
-        }
-    }   else {
-            return `Try typing (rock, paper or scissors)`
-    }
-     
-}
-// keeping score and asking user for playerSection.//
-
 function game() {
+    // ADDING BUTTONS TO PLAYER SECTION //
+    const rockButton = document.querySelector('#rock');
+    const paperButton = document.querySelector('#paper');
+    const scissorsButton = document.querySelector('#scissors');
+    //   Selecting a div to keep the results
+    const resultDiv = document.querySelector('#result');
+
+    //   Selecting another div to show the scores
+    const scoreDiv = document.querySelector('#scores');
+
+    // Who won?
+    const winner = document.querySelector('#win');
+
+    // Setting Default values for the points
     let userPoint = 0;
     let computerPoint = 0;
+    let playerSection = '';
 
-    for (let i = 0; i < 5; i++){
-    const playerSection = prompt(`Enter your choice (rock, paper, or scissors)`);
-    const computerSection = getComputerChoice();
-    const roundResult = singleRound(playerSection, computerSection);
-    
-    if (roundResult.includes('You win!')){
-        userPoint++;
-    } else if (roundResult.includes('You lose!')){
-        computerPoint++;
+    // Calling button's function
+    rock(rockButton);
+    paper(paperButton);
+    scissors(scissorsButton);
+
+        console.log(a), false
+
+    var buton = document.getElementsByClassName('buton');
+    buton.addEventListener('buton', (a) => {
+        console.log(a)}, false);
+    console.log(buton[0])
+
+    /////BUTTON FUNCTIONS////
+
+    function rock(rockButton) {
+        rockButton.addEventListener('click', () => {
+            playerSection = 'rock';
+            result();
+        });
     }
-    console.log(roundResult);
-}    
-    return `Your point: ${userPoint} and computer point: ${computerPoint}`;
+    function paper(paperButton) {
+        paperButton.addEventListener('click', () => {
+            playerSection = 'paper';
+            result();
+        });
+    }
+
+    function scissors(scissorsButton) {
+        scissorsButton.addEventListener('click', () => {
+            playerSection = 'scissors';
+            result();
+        });
+    }
+
+
+    function result() {
+        let result = singleRound(playerSection, getComputerChoice()); //Result's declaration
+        if (userPoint === 5 || computerPoint === 5) {
+            rockButton.disabled = true;
+            paperButton.disabled = true;
+            scissorsButton.disabled = true;
+            if (userPoint > computerPoint) {
+                winner.textContent = `Congratulations, YOU WON!`;
+            } else if (computerPoint > userPoint) {
+                winner.textContent = `Oh unlucky :( Wishing you better luck next time`;
+            }
+        }
+        resultDiv.textContent = result;
+        scoreDiv.textContent = `Your point: ${userPoint}  computer point: ${computerPoint}`;
+    }
+
+    function singleRound(playerSection, computerSection) {
+    if (playerSection === 'rock') {
+        if (computerSection === 'scissors') {
+            userPoint += 1;
+            return `You win! ${playerSection} beats ${computerSection}`;
+        } else if (computerSection === 'rock') {
+            return `You picked ${playerSection} and computer picked ${computerSection}. It\'s a TIE!`;
+        } else {
+            computerPoint++;
+            return `You lose! ${computerSection} beats ${playerSection}`;
+        }
+    }
+    else if (playerSection === 'scissors') {
+        if (computerSection === 'scissors') {
+            return `You picked ${playerSection} and computer picked ${computerSection}. It\'s a TIE!`;
+        } else if (computerSection === 'paper') {
+            userPoint += 1;
+            return `You win! ${playerSection} beats ${computerSection}`;
+        } else {
+            computerPoint++;
+            return `You lose! ${computerSection} beats ${playerSection}`;
+        }
+    }
+    else if (playerSection === 'paper') {
+        if (computerSection === 'paper') {
+            return `You picked ${playerSection} and computer picked ${computerSection}. It\'s a TIE!`;
+        } else if (computerSection === 'rock') {
+            userPoint += 1;
+            return `You win! ${playerSection} beats ${computerSection}`;
+        } else {
+            computerPoint++;
+            return `You lose! ${computerSection} beats ${playerSection}`;
+        }
+    }
+
 }
 
-console.log(game());
+}
+
+game();
